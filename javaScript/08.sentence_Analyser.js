cda988bc52d54cb50e39ca99dc1b905ebf85ac65
Rajkumar <vkrajkumar1996@gmail.com>
2026-07-11 20:30:14 +0530

    write a vowels counting Code

---
+++ javaScript/08.sentence_Analyser.js
@@ -0,0 +1,11 @@
+function getVowelCount(sentence) {
+  const vowels = "aeiou";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
